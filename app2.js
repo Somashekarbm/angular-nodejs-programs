19f6d076289c859218c0436d1a5d563f22d585ba
BM SOMASHEKAR <121337385+Somashekarbm@users.noreply.github.com>
2024-03-06 07:14:40 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -7,3 +7,36 @@ app.get('/',(req,res)=>{
 app.listen(port,()=>{
 	console.log(`server running at http://localhost:${port}/`);
 });
+
+//current program
+const http = require('http');
+
+
+
+// Create a server object
+
+const server = http.createServer((req, res) => {
+
+    // Set the response HTTP header with HTTP status and Content type
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+
+
+    // Send the response body "Hello from Node.js Server!"
+
+    res.end('Hello from Node.js Server!\n');
+
+});
+
+
+
+// Server listens on port 3000
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+
+    console.log(`Server running at http://localhost:${PORT}/`);
+
+});
