18e50648fc4695365dd6fcd5add3b5747af91f95
BM SOMASHEKAR <121337385+Somashekarbm@users.noreply.github.com>
2024-03-06 07:06:43 +0530

    Update program7.js

--- program7.js
+++ program7.js
@@ -1,4 +1,4 @@
 const currentDate=new Date();
 //console.log(`current date and time : ${currentDate}`);
-const formatdatetime=`${currentDate.getFullYear()}-${currentDate.getMonth()+1).toString().padStart(2,'0')}-${currentDate.getDate().toString().padStart(2,'0')}${currentDate.getHours().toString().padStart(2,'0')}:${currentDate.getMinutes().toString().padStart(2,'0')}:${currentDate.getSeconds().toString().padStart(2,'0')}`;
+const formatdatetime=`${currentDate.getFullYear()}-${(currentDate.getMonth()+1).toString().padStart(2,'0')}-${currentDate.getDate().toString().padStart(2,'0')}${currentDate.getHours().toString().padStart(2,'0')}:${currentDate.getMinutes().toString().padStart(2,'0')}:${currentDate.getSeconds().toString().padStart(2,'0')}`;
 console.log(`Formatted date and time:${formatdatetime}`);
